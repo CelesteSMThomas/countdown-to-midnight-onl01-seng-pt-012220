bfea3a42788c46bcdd8db8489c0fca4ffb909bab
Celeste Thomas <celestesmthomas@gmail.com>
2020-01-29 15:14:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(number)
 
-def countdown
+  while number > 0
+    number -= 1
+    puts "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sheep(num_secs)
+  until num_secs == 5
+  num_secs += 1
+  sleep 1
+  end
+end
+
+    
